4c810111eaa0c3c15d89198022412ec6620572f2
z-ph <you@example.com>
2025-07-06 22:54:39 +0800

    修改静态托管路径

--- todo-back/src/index.ts
+++ todo-back/src/index.ts
@@ -5,7 +5,7 @@ import todoRouter from "./routes/todo";
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static("../todo-front/dist"));
 // monitor req
 app.use((req, res, next) => {
   console.log(`[${req.method}] ${req.url}`);
